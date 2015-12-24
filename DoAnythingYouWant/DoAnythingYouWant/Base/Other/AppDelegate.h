43c5649001e909fd12b76341d6d7394f92d809f1
蓝斯 <>
2015-12-24 10:51:08 +0800

    new project of DoAnythingYouWant

---
+++ DoAnythingYouWant/DoAnythingYouWant/Base/Other/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DoAnythingYouWant
+//
+//  Created by zgjun on 15/12/24.
+//  Copyright © 2015年 barry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DoAnythingYouWant/DoAnythingYouWant/Main/Controllers/BaseNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  BaseNavigationController.h
+//  DoAnythingYouWant
+//
+//  Created by zgjun on 15/12/24.
+//  Copyright © 2015年 barry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseNavigationController : UINavigationController
+
+@end
